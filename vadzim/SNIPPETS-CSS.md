22c0c5d5cf8b60ec46447373b78ed0ae4c1e3107
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-03-31 15:35:11 +0200

    functions

--- vadzim/SNIPPETS-CSS.md
+++ vadzim/SNIPPETS-CSS.md
@@ -2,4 +2,10 @@
 
 | snippet | effect |
 | --- | --- |
+| bcc | background-color: #fff |
+| m10p30e5x | margin: 10% 30em 5ex; |
+| pos:a | position: absolute |
+| pos:f | position: fixed |
+| pos:r | position: relative |
 | pos:s | position: static |
+| w100p | width: 100% |
--- vadzim/theory-app/js-syntax.js
+++ vadzim/theory-app/js-syntax.js
@@ -1,5 +1,5 @@
 let myString
-myString = "'HochkommaString'"
+myString = 'HochkommaString'
 let stringMitHTML = '<b>bold text</b>'
 
 /* AUSDRUCK vs ANWEISUNG */
@@ -9,11 +9,19 @@ console.log('text');
 
 /* document.getElementById('par_iT').innerText = myString // HochkommaString
 document.getElementById('par_iH').innerHTML = myString // HochkommaString
-document.getElementById('out_v').value = myString // HochkommaString */
+document.getElementById('out_v').value = myString // HochkommaString
+document.getElementById('out_iT').innerText = myString // HochkommaString */
+
 
 document.getElementById('par_iT').innerText = stringMitHTML // <b>bold text</b>
 document.getElementById('par_iH').innerHTML = stringMitHTML // bold text
 document.getElementById('out_v').value = stringMitHTML // <b>bold text</b>
+document.getElementById('out_iT').innerText = stringMitHTML
 
 let par = document.getElementById('par_iT')
-console.log('par :', par);
+console.log('par :', par)
+console.log(par);
+console.info('Info FYI')
+console.error('Fehler in der Zeile 25: ', par);
+
+
